f53871ca90fe5c41373b127dd8f27914875903ec
1 <1>
2023-09-15 14:47:00 +0700

    Init

---
+++ lab1.cpp
@@ -0,0 +1,28 @@
+#include "lab1.hpp"
+
+int main() {
+    long long int a = 59666548, x = 343434345, p = 89;
+    string x1 = binary(x);
+    cout << modPow(a, x1, p);
+}
+
+string binary(long long number) {
+    string x1 = number == 0 ? to_string(number) : "";
+    while (number > 0) {
+        int num_tmp = number % 2;
+        x1 = x1 + to_string(num_tmp);
+        x /= 2;
+    }
+    return x1;
+}
+
+long long modPow(long long a, string x, long long p) {
+    int t = x.size();
+    cout << x << endl;
+    long long y = 1;
+    for (int i = 0; i < t; i++) {
+        if (x1[i] == '1') y = (y * a) % p;
+        a = (a * a) % p;
+    }
+    return y;
+}
---
+++ lab1.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+string binary(long long number);
+long long modPow(long long a, string x, int p);
